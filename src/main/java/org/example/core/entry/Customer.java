00ae63bc90248df7f55934ecfe3796defeafddff
Пустовойт Павел Александрович <PAPustovoyt.SBT@sberbank.ru>
2016-11-11 11:58:47 +0300

    Added Customer dto class

---
+++ src/main/java/org/example/core/entry/Customer.java
@@ -0,0 +1,25 @@
+package org.example.core.entry;
+
+import java.util.UUID;
+
+public class Customer {
+
+    private UUID id;
+    private String name;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
