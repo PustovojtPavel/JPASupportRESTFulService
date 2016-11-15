912453b828fced1b932a16f9ec83931a24f99b91
Пустовойт Павел Александрович <PAPustovoyt.SBT@sberbank.ru>
2016-11-15 18:47:06 +0300

    added autowired anotation

--- src/main/java/org/example/core/service/impl/CustomerServiceImpl.java
+++ src/main/java/org/example/core/service/impl/CustomerServiceImpl.java
@@ -3,6 +3,7 @@ package org.example.core.service.impl;
 import org.example.core.dao.CustomerDAO;
 import org.example.core.entry.Customer;
 import org.example.core.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,6 +11,7 @@ import java.util.List;
 @Service
 public class CustomerServiceImpl implements CustomerService {
 
+    @Autowired
     private CustomerDAO customerDAO;
 
     public List<Customer> getAll() {
--- src/main/java/org/example/web/rest/GetCustomerController.java
+++ src/main/java/org/example/web/rest/GetCustomerController.java
@@ -2,7 +2,10 @@ package org.example.web.rest;
 
 import org.example.core.entry.Customer;
 import org.example.core.service.CustomerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -12,8 +15,10 @@ import java.util.List;
 
 @Path("/get")
 @Produces(MediaType.APPLICATION_JSON + GetCustomerController.CHARSET_UTF8)
+@Component
 public class GetCustomerController {
 
+    private static final Logger log = LoggerFactory.getLogger(GetCustomerController.class);
     final static String CHARSET_UTF8 = ";charset=UTF-8";
 
     @Autowired
